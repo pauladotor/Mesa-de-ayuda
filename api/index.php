93f71c87cf4fca85462421f754a778bc1e28ff8c
pauladotor <pauladotor9@gmail.com>
2025-10-01 15:35:21 -0500

    fix: Corregida la ruta de enrutamiento 404 en Vercel

--- api/index.php
+++ api/index.php
@@ -1,5 +1,6 @@
 <?php
-// Este archivo sirve como punto de entrada para Vercel.
-header('Location: /TecDesarrollo/public');
+// Este archivo redirige a la página principal (index.php) dentro de la carpeta Home
+// Usamos el código de estado 302 para la redirección
+header('Location: /TecDesarrollo/public/view/Home/index.php', true, 302);
 exit();
 ?>
